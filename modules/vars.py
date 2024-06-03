92f028070b4e721497c32e481969a744787b6211
mohitbooraget <137650003+mohitbooraget@users.noreply.github.com>
2024-06-03 13:16:10 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -2,7 +2,7 @@ import os
 
 api_id = int(os.environ.get("API_ID", '27862677'))
 api_hash = os.environ.get("API_HASH", '4b11dd648188731fb7c9bc8083e8791c')
-bot_token = os.environ.get("BOT_TOKEN", '7129448604:AAHBtE1PZG2YGutDVNZ_pgH5E_1UNW9BsDo')
+bot_token = os.environ.get("BOT_TOKEN", '7160005914:AAGf0_a89XgHZXyR4fktbOzYGf5XrJVkSo4')
 
 
 
